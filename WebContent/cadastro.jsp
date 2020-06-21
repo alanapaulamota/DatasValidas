a3b9a5cfd45e119f75fc12ba181d9fbd3595abf5
alana <alana@alana>
2020-06-21 19:38:31 -0300

    inicializando CRUD

---
+++ WebContent/cadastro.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>LoginServlet</title>
+</head>
+<body>
+	<h1>CRUD JSP</h1>
+<!-- 	<form action="LoginServlet" method="post"> -->
+		<h3> Cadastro</h3>
+		<table border="1">
+		<tr>
+		<td>Nome</td>
+		<td>DataNascimento</td>
+		<td>Email</td>
+		<td>Senha</td>
+		</tr>
+
+<!-- 	</form> -->
+</body>
+</html>
---
+++ src/com/alana/web/servlet/conn/MySqlConnect.java
@@ -0,0 +1,28 @@
+package com.alana.web.servlet.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnect {
+
+	public static Connection getMySQLConnection() throws ClassNotFoundException, SQLException {
+		String hostName = "localhost";
+		String dbName = "CRUD";
+		String userName = "root";
+		String password = "123";
+		return getMySQLConnection(hostName, dbName, userName, password);
+	}
+
+	public static Connection getMySQLConnection(String hostName, String dbName, String userName, String password)
+			throws SQLException, ClassNotFoundException {
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+		Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+		return conn;
+	}
+
+}
